badd243cd51a414e46d06ba9abcfed3f12bed782
gremble0 <hermanstornes@gmail.com>
2024-10-02 10:02:03 +0200

    Use tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,8 @@
-import "./App.css";
-
-export default function App() {}
+export default function App() {
+  return (
+    <div className="content-center bg-red-900">
+      <p>1</p>
+      <p>2</p>
+    </div>
+  );
+}
