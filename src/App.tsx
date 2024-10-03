df750916adf6a4afe78087ed3abd1d64b2d858e1
gremble0 <hermanstornes@gmail.com>
2024-10-03 10:23:07 +0200

    Start react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import Body from "./components/Body";
+import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
+import NavBar from "./components/NavBar";
+import Posts from "./components/posts/Posts";
 
 export default function App() {
   return (
     <>
       <Header />
-      <Body />
+      <div className="flex space-x-4">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Posts />} />
+        </Routes>
+      </div>
     </>
   );
 }
